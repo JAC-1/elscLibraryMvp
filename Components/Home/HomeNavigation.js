8aebbad955f6861efcf462f4b20a8c5a2da4f620
JAC-1 <79427501+JAC-1@users.noreply.github.com>
2023-03-27 09:16:13 +0900

    Add components folder

---
+++ Components/Home/HomeNavigation.js
@@ -0,0 +1,7 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import BooksHistoryScreen from "./screens/BooksHistoryScreen";
+import BooksOutScreen from "./screens/BooksOutScreen";
